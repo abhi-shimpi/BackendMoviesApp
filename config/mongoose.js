91b11c1419cd34dda9d21e65e85aec1ba4fc0771
abhi-shimpi <abhimanshimpi@gmail.com>
2024-04-28 22:59:13 +0530

    Fixed mongo bug

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGODB_URI;
+const mongoURI = "mongodb://localhost:27017/movieapp";
 
 mongoose.connect(mongoURI,{useNewUrlParser: true});
 
